93f99ae74cdbd0d34c56370fafbf5792dccb9af6
XayZay <asaoluoluwasoromidayo@gmail.com>
2023-12-21 23:19:41 +0000

    Some small changes

--- src/App.js
+++ src/App.js
@@ -12,26 +12,16 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 function App() {
   const [input, setInput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-  
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// In this section, we set the user authentication, user and app ID, model details, and the URL
-// of the image we want as an input. Change these strings to run your own example.
-//////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Your PAT (Personal Access Token) can be found in the portal under Authentification
 const PAT = 'e18c874388ed4378bb0446a97e794be1';
-// Specify the correct user_id/app_id pairings
-// Since you're making inferences outside your app's scope
+
 const USER_ID = 'clarifai';       
 const APP_ID = 'main';
-// Change these to whatever model and image URL you want to use
+
 const MODEL_ID = 'face-detection';
 const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';   
 
 
-///////////////////////////////////////////////////////////////////////////////////
-// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-///////////////////////////////////////////////////////////////////////////////////
 
 const raw = JSON.stringify({
     "user_app_id": {
@@ -58,9 +48,7 @@ const requestOptions = {
     body: raw
 };
 
-// NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-// https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-// this will default to the latest version_id
+
 const onButtonSubmit = () => {
   fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
     .then(response => response.json())
